69bdb9be00d7dc6c0e2d504fa932708b5dd48790
Emily <36363495+jonasjaguar@users.noreply.github.com>
2020-12-11 12:19:00 +0200

    add force versions of reboot/poweroff

--- ichirou.c
+++ ichirou.c
@@ -13,8 +13,10 @@
 #define TIMEO   30
 
 static void sigpoweroff(void);
+static void sigfpoweroff(void);
 static void sigreap(void);
 static void sigreboot(void);
+static void sigfreboot(void);
 static void spawn(char *const []);
 static void spawnwait(char *const argv[]);
 
@@ -22,10 +24,12 @@ static struct {
     int signal;
     void (*handler)(void);
 } signalmap[] = {
-    { SIGUSR1, sigpoweroff },
-    { SIGCHLD, sigreap     },
-    { SIGALRM, sigreap     },
-    { SIGINT,  sigreboot   },
+    { SIGUSR1, sigpoweroff  },
+    { SIGUSR2, sigfpoweroff },
+    { SIGCHLD, sigreap      },
+    { SIGALRM, sigreap      },
+    { SIGINT,  sigreboot    },
+    { SIGTERM, sigfreboot   },
 };
 
 #include "config.h"
@@ -69,6 +73,12 @@ int main(void) {
     return 0;
 }
 
+/**
+ * this safely powers off the system
+ * it stops services, asks processes to terminate,
+ * kills non-terminating processes, syncs filesystems,
+ * runs shutdown rcfile and then powers off the system.
+**/
 static void sigpoweroff(void) {
     /* stop services */
     spawnwait(servstopcmd);
@@ -99,12 +109,47 @@ static void sigpoweroff(void) {
         sleep(1);
 }
 
+/**
+ * this powers off the system with force level 1
+ * it kills all processes, syncs filesystems,
+ * runs the shutdown rcfile and then powers off the system.
+**/
+static void sigfpoweroff(void) {
+    /* kill all processes */
+    kill(-1, SIGKILL);
+    sigreap();
+
+    /* sync filesystems */
+    sync();
+
+    /* run rc.shutdown */
+    spawnwait(rcshutdownfile);
+    sigreap();
+
+    /* power off system */
+    if (vfork() == 0) {
+        reboot(RB_POWER_OFF);
+        _exit(EXIT_SUCCESS);
+    }
+    while (1)
+        sleep(1);
+}
+
+/**
+ * this reaps child processes
+**/
 static void sigreap(void) {
     /* reap children */
     while (waitpid(-1, NULL, WNOHANG) > 0);
     alarm(TIMEO);
 }
 
+/**
+ * this safely reboots the system.
+ * it stops services, asks processes to terminate,
+ * kills non-terminating processes, syncs filesystems,
+ * runs shutdown rcfile and then reboots the system.
+**/
 static void sigreboot(void) {
     /* stop services */
     spawnwait(servstopcmd);
@@ -116,6 +161,32 @@ static void sigreboot(void) {
 
     /* after SIGKILLTIMEOUT seconds, kill all processes */
     sleep(SIGKILLTIMEOUT);
+    kill(-1, SIGKILL);
+    sigreap();
+
+    /* sync filesystems */
+    sync();
+
+    /* run rc.shutdown */
+    spawnwait(rcshutdownfile);
+    sigreap();
+
+    /* reboot system */
+    if (vfork() == 0) {
+        reboot(RB_AUTOBOOT);
+        _exit(EXIT_SUCCESS);
+    }
+    while (1)
+        sleep(1);
+}
+
+/**
+ * this reboots the system with force level 1
+ * it kills all processes, syncs filesystems,
+ * runs the shutdown rcfile and then reboots the system.
+**/
+static void sigfreboot(void) {
+    /* kill all processes */
     kill(-1, SIGKILL);
     sigreap();
 
